9dd0a72d57aabc5230891e54576ba849547a483b
inathan44 <inathan44@yahoo.com>
2023-03-09 16:16:35 -0500

    blinking cursor, more experiments

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,39 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
 const InputForm = () => {
-  // const [typeTest, setTypeTest] = useState<string>(
+  // const [quoteToType, setQuoteToType] = useState<string>(
   //   'Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss will gaze back into you.'
   // );
-  const [typeTest, setTypeTest] = useState<string>(
+  const [quoteToType, setQuoteToType] = useState<string>(
     'This is a typing test so type faster'
   );
-  const [textForm, setTextForm] = useState<string>('');
+  const [userTextInput, setUserTextInput] = useState<string>('');
   const [testComplete, setTestComplete] = useState<boolean>(false);
 
   const letterColor = (idx: number): string => {
-    if (idx > textForm.length - 1) return '#55848a';
+    if (idx > userTextInput.length - 1) return '#55848a';
     else {
-      return typeTest[idx] === textForm[idx] ? 'white' : 'red';
+      return quoteToType[idx] === userTextInput[idx] ? 'white' : 'red';
     }
   };
 
   useEffect(() => {
-    setTestComplete(typeTest.length === textForm.length);
-  }, [typeTest, textForm]);
+    setTestComplete(quoteToType.length === userTextInput.length);
+  }, [quoteToType, userTextInput]);
 
-  console.log(testComplete);
+  useEffect(() => {
+    console.log('userTextInput', userTextInput.length);
+    console.log('next letter', quoteToType[userTextInput.length]);
+    console.log('userTextInput', userTextInput);
+  }, [userTextInput]);
 
   return (
-    <div className='flex pt-44 flex-col items-center gap-4'>
+    <div className="flex pt-44 flex-col items-center gap-4">
       <h1 style={{ visibility: testComplete ? 'visible' : 'hidden' }}>
         Test Complete
       </h1>
-      <div className='relative border-2 px-8 py-4 rounded-md text-3xl'>
+      <div className="relative border-2 px-8 py-4 rounded-md text-3xl">
         <h2>
-          {typeTest.split('').map((char, idx) => (
+          {quoteToType.split('').map((char, idx) => (
             <span
               className={
-                idx === textForm.length - 1
+                idx === userTextInput.length - 1
                   ? "after:content-['|'] after:animate-[pulse_2s_infinite] after:text-purple-500"
                   : ''
               }
@@ -47,16 +51,16 @@ const InputForm = () => {
           ))}
         </h2>
         <textarea
-          className='border-2 border-white opacity-0 w-full h-full text-2xl rounded absolute py-4 px-8 left-0 top-0'
+          className="border-2 border-white opacity-0 w-full h-full text-2xl rounded absolute py-4 px-8 left-0 top-0"
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-            setTextForm(e.target.value);
+            setUserTextInput(e.target.value);
           }}
-          value={textForm}
+          value={userTextInput}
         />
       </div>
       <button
         onClick={() => {
-          setTextForm('');
+          setUserTextInput('');
         }}
       >
         Reset Test
