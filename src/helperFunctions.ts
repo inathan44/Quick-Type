69f69bf34382f790627900eb432b5064c8a9ba35
inathan44 <inathan44@yahoo.com>
2023-03-10 13:53:30 -0500

    Moved functions to own file

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,3 +1,59 @@
-const string: string = 'hi';
+interface KeyLogic {
+  currentWordNumber: number;
+  userInputWordLength: number;
+  quoteWordLength: number;
+  quoteWord: string;
+  userTypedWord: string;
+  reassignWord: string;
+  splitQuote: string[];
+}
 
-export default string;
+function deleteExcessLettersData(
+  userTextInput: string,
+  duplicateQuoteToType: string,
+  quoteToType: string
+): KeyLogic {
+  const currentWordNumber = userTextInput.split(' ').length;
+  const userInputWordLength =
+    userTextInput.split(' ')[currentWordNumber - 1].length;
+  const quoteWordLength =
+    duplicateQuoteToType.split(' ')[currentWordNumber - 1].length;
+  const quoteWord = duplicateQuoteToType.split(' ')[currentWordNumber - 1];
+  const userTypedWord = userTextInput
+    .split(' ')
+    [currentWordNumber - 1].slice(
+      0,
+      userTextInput.split(' ')[currentWordNumber - 1].length - 1
+    );
+  const reassignWord = quoteWord.concat(
+    userTypedWord.slice(quoteWordLength, userInputWordLength - 1)
+  );
+  const splitQuote = quoteToType.split(' ');
+  return {
+    currentWordNumber,
+    userInputWordLength,
+    quoteWordLength,
+    quoteWord,
+    userTypedWord,
+    reassignWord,
+    splitQuote,
+  };
+}
+
+function remakeQuoteString(
+  userTextInput: string,
+  duplicateQuoteToType: string,
+  quoteToType: string
+): string {
+  const logicData = deleteExcessLettersData(
+    userTextInput,
+    duplicateQuoteToType,
+    quoteToType
+  );
+  const splitQuote = logicData.splitQuote;
+  // Replacing the current word with what was deleted from user input
+  splitQuote[logicData.currentWordNumber - 1] = logicData.reassignWord;
+  return splitQuote.join(' ');
+}
+
+export { deleteExcessLettersData, remakeQuoteString };
