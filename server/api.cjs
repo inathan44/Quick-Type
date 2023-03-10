6fe4a98ef47dbd01e4717bb00da464fe57e0ac19
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-10 13:33:05 -0600

    Error handling

--- server/api.cjs
+++ server/api.cjs
@@ -1,17 +1,15 @@
 const router = require('express').Router();
-const {User, Score} = require('./db/index.cjs')
+const { User, Score } = require('./db/index.cjs');
 
 router.post('/signup', async (req, res, next) => {
-    try {
-    const newUser = await User.create(req.body)
-    res.send(newUser)
-    } catch (err) {
-        console.error(err)
-}
-})
+  try {
+    const newUser = await User.create(req.body);
+    res.send(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/saveTypingData', (req, res, next) => {
-
-})
+// router.get('/addTypingStats', (req, res, next) => {});
 
 module.exports = router;
--- server/index.cjs
+++ server/index.cjs
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const dotenv = require('dotenv').config;
-const  cors = require('cors');
+const cors = require('cors');
 
 const PORT = 3030;
 app.use(express.json());
 app.use(cors());
-app.use('/api', require('./api.cjs'))
+app.use('/api', require('./api.cjs'));
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status(500).send(err.message('internal server error')));
+});
 
 async function init() {
   app.listen(PORT, () => {
