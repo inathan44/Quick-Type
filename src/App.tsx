a699690d056f83bd3cd2d2e481ca37d8e4d60a6c
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-10 17:22:50 -0600

    Login added to APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './index.css';
 import InputForm from './components/InputForm';
-import Signup from './components/Signup';
+import SignupForm from './components/SignupForm';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     // <div className='text-white'>
-      // <Signup />
-      <InputForm />
+    // <SignupForm />
+    // <InputForm />
+    <LoginForm />
     // </div>
   );
 }
