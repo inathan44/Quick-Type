821b3ec03589449a571f14133fab1a21e40ba6fd
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-10 17:19:07 -0600

    future middleWare

---
+++ server/authMIdWare.cjs
@@ -0,0 +1,22 @@
+const { User } = require('./db');
+
+// const requireTokenAuth = async (req, res, next) => {
+//   try {
+//     const token = req.cookies.token;
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// const isAdmin = async (req, res, next) => {
+//   try {
+//     //
+//   } catch (err) {
+//     next(err);
+//   }
+//  };
+
+// module.exports = {
+//     requireTokenAuth,
+//     isAdmin
+// }
