ece4763b518edb2ba0588f7658b32ee3d99de655
inathan44 <inathan44@yahoo.com>
2023-03-10 17:24:58 -0500

    added stat slice into store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthSlice from './slices/AuthSlice';
+import StatSlice from './slices/StatSlice';
 import TypeInputSlice from './slices/TypeInputSlice';
 
 const store = configureStore({
   reducer: {
     auth: AuthSlice,
-    typeInput: TypeInputSlice
+    typeInput: TypeInputSlice,
+    statSlice: StatSlice,
   },
-})
+});
 
 export default store;
 
