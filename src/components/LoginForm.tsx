b0467018a2ec86fcc6f265c80be1eb4a2050b818
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-10 17:23:02 -0600

    LoginForm page added

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, SyntheticEvent } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    axios.post('http://localhost:3030/api/login', { username, password });
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h1 className="text-white block">LOGIN </h1>
+      <form id="form1" onSubmit={handleSubmit}>
+        <label className="text-white block"> USERNAME</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label className="text-white block"> PASSWORD</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </form>
+      <button type="submit" form="form1" value="Submit" className="text-white">
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default LoginForm;
