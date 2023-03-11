51405efa154db587fe41f406c9e01af505ca987b
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-10 23:13:32 -0600

    more seed data

--- seed.cjs
+++ seed.cjs
@@ -1,23 +1,60 @@
-const db = require('./server/db/database.cjs');
-const { User, Score } = require('./server/db/index.cjs');
+// const db = require('./server/db/database.cjs');
+'use strict';
+const { User, Score, db } = require('./server/db/index.cjs');
+
+const Users = [
+  {
+    username: 'EasyName',
+    email: 'EasyName@gmail.com',
+    password: 'EasyName',
+  },
+  {
+    username: 'MidTierIan',
+    email: 'AlwaysMidTier@midTier.com',
+    password: 'AndrewDaBest',
+  },
+  {
+    username: 'GrettaThunberg',
+    email: 'randomAccount@gmail.com',
+    password: 'OwOUwUOwO',
+  },
+];
+const Scores = [
+  {
+    WPM: 10,
+    type: 'english',
+    time: 100,
+    incorrectLetters: 0,
+    lettersTyped: 100,
+    userId: 1,
+  },
+  {
+    WPM: 90,
+    type: 'english',
+    time: 100,
+    incorrectLetters: 0,
+    lettersTyped: 100,
+    userId: 1,
+  },
+];
 
 const seed = async () => {
   try {
-    await db.sync();
+    await db.sync({ force: true });
     console.log('seeeeeeding the Database <><><>><><><><><><><><><>');
-    await User.create({
-      username: 'EasyName',
-      email: 'mother@gmail.com',
-      password: 'EasyName',
-    });
-    await Score.create({
-      WPM: 10,
-      type: 'english',
-      time: 100,
-      incorrectLetters: 0,
-      lettersTyped: 100,
-      userId: 1,
-    });
+
+    await Promise.all(
+      Users.map((user) => {
+        return User.create(user);
+      })
+    );
+    await Promise.all(
+      Scores.map((score) => {
+        // console.log('<><><><><><><>', score);
+        return Score.create(score);
+      })
+    );
+
     db.close();
   } catch (err) {
     console.error(err);
