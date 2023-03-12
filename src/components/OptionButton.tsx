b3c68dfa4cfd5e0a07b4f0c7b3d7f5735455cf34
inathan44 <inathan44@yahoo.com>
2023-03-12 19:46:51 -0400

    Option selector menu components

---
+++ src/components/OptionButton.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, MouseEventHandler } from 'react';
+import { Languages, Mode } from './OptionsMenu';
+import { selectUseCountdown, changeMode } from '../store/slices/StatSlice';
+import { useAppDispatch } from '../store/hooks';
+import { useDispatch } from 'react-redux';
+
+interface ButtonProps {
+  children: ReactNode;
+  clickFunc: any;
+  id: Mode | Languages | number;
+}
+
+const OptionButton = (props: ButtonProps) => {
+  const dispatch = useDispatch();
+
+  return (
+    <button
+      onClick={() => {
+        props.clickFunc(props.id);
+      }}
+      className="border-2 px-2 py-1 rounded-lg text-sm text-gray-300 border-gray-300"
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default OptionButton;
