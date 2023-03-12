285dba4e42dbc0fedc74d127abeeafe93af01a8f
inathan44 <inathan44@yahoo.com>
2023-03-11 23:56:30 -0500

    Display navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import './index.css';
 import InputForm from './components/InputForm';
 import Signup from './components/Signup';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     // <div className='text-white'>
-    <Routes>
-      <Route path="signup" element={<Signup />} />
-      <Route path="/" element={<InputForm />} />
-    </Routes>
+    <div className="max-w-7xl mx-auto">
+      <NavBar />
+      <Routes>
+        <Route path="signup" element={<Signup />} />
+        <Route path="/" element={<InputForm />} />
+      </Routes>
+    </div>
     // </div>
   );
 }
