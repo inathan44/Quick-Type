fcde681b6ba1062523bbcf14d8e6aa0944718a3b
inathan44 <inathan44@yahoo.com>
2023-03-13 11:26:48 -0400

    stats don't require db

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useAppSelector } from '../store/hooks';
-import { selectLastTest } from '../store/slices/StatSlice';
+import {
+  selectLastTest,
+  selectWpm,
+  selectAccuracy,
+  selectIncorrectKeys,
+} from '../store/slices/StatSlice';
 
 const Results = () => {
   const lastTest = useAppSelector(selectLastTest) || null;
+  const wpm = useAppSelector(selectWpm);
+  const accuracy = useAppSelector(selectAccuracy);
+  const incorrectKeys = useAppSelector(selectIncorrectKeys);
 
-  if (!lastTest) return <h1>Loading...</h1>;
+  // if (!lastTest.ra) return <h1>Loading...</h1>;
 
   return (
     <section className="text-gray-300">
@@ -13,24 +21,22 @@ const Results = () => {
       <div className="flex text-black justify-between w-11/12 mx-auto">
         <div className="w-60 h-60 bg-gray-300 rounded-2xl opacity-75 flex flex-col items-center gap-6 justify-center hover:bg-white  ease-in transition-all">
           <h2 className="font-bold text-5xl">WPM</h2>
-          <p className="text-4xl font-bold text-yellow-600">{lastTest?.wpm}</p>
+          <p className="text-4xl font-bold text-yellow-600">{wpm}</p>
         </div>
         <div className="w-60 h-60 bg-gray-300 rounded-2xl opacity-75 flex flex-col items-center gap-6 justify-center hover:bg-white  ease-in transition-all">
           <h2 className="font-bold text-5xl">Accuracy</h2>
           <p className="text-4xl font-bold text-green-600">
-            {lastTest ? lastTest.accuracy * 100 : 'N/A'}%
+            {accuracy * 100 || 'N/A'}%
           </p>
         </div>
         <div className="w-60 h-60 bg-gray-300 rounded-2xl opacity-75 flex flex-col items-center gap-6 justify-center hover:bg-white  ease-in transition-all">
           <h2 className="font-bold text-5xl">Errors</h2>
-          <p className="text-4xl font-bold text-red-500">
-            {lastTest?.incorrectKeys}
-          </p>
+          <p className="text-4xl font-bold text-red-500">{incorrectKeys}</p>
         </div>
         <div className="w-60 h-60 bg-gray-300 rounded-2xl opacity-75 flex flex-col items-center gap-6 justify-center hover:bg-white  ease-in transition-all">
           <h2 className="font-bold text-5xl">Raw</h2>
           <p className="text-4xl font-bold text-sky-700">
-            {lastTest ? lastTest.raw : 'N/A'}
+            {lastTest ? lastTest.raw : 'Loading...'}
           </p>
         </div>
       </div>
