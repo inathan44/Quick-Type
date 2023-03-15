6264a2cb0b7595cb5f95fc51121986b29e46c06e
inathan44 <inathan44@yahoo.com>
2023-03-15 16:22:33 -0400

    Changed loading to calc...

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -36,7 +36,7 @@ const Results = () => {
         <div className="w-60 h-60 bg-gray-300 rounded-2xl opacity-75 flex flex-col items-center gap-6 justify-center hover:bg-white  ease-in transition-all">
           <h2 className="font-bold text-5xl">Raw</h2>
           <p className="text-4xl font-bold text-sky-700">
-            {lastTest ? lastTest.raw : 'Loading...'}
+            {lastTest ? lastTest.raw : 'Calculating...'}
           </p>
         </div>
       </div>
