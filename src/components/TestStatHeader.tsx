6157595fa724c0648abbfd5c645945fd891819b0
inathan44 <inathan44@yahoo.com>
2023-04-09 23:37:34 -0400

    Round timer to nearest whole number

--- src/components/TestStatHeader.tsx
+++ src/components/TestStatHeader.tsx
@@ -98,14 +98,12 @@ const TestStatHeader = () => {
     <div className="flex justify-center text-white gap-16 items-center">
       <div className="flex flex-col items-center">
         <h3 className="text-2xl">WPM</h3>
-        <p className="text-green-400 text-xl">
-          {wpm === Infinity ? 'Calc' : wpm}
-        </p>
+        <p className="text-green-400 text-xl">{wpm === Infinity ? '0' : wpm}</p>
       </div>
       <div className="flex flex-col items-center text-4xl">
         <h3>{useCountdown ? 'Time Remaining' : 'Time Elapsed'}</h3>
         <p className="text-yellow-400">
-          {useCountdown ? countdownTimer : timeElapsed}
+          {useCountdown ? Math.floor(countdownTimer) : Math.floor(timeElapsed)}
         </p>
       </div>
       <div className="flex flex-col items-center">
