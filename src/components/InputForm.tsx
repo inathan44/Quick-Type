9072d56a6b845ed74067ad044f98e68a81505a6e
inathan44 <inathan44@yahoo.com>
2023-04-10 14:20:27 -0400

    Implemented exported keyPress func

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -165,157 +165,25 @@ const InputForm = () => {
   // KEY PRESS FUNCTION BELOW, handles key logic, colors, etc
 
   function handleKeyPress(e: React.KeyboardEvent<HTMLTextAreaElement>): void {
-    setLastKeyPressed(e.key);
-    const logicData = deleteExcessLettersData(
+    keyPress(
+      e,
+      setLastKeyPressed,
+      deleteExcessLettersData,
       userTextInput,
       duplicateQuoteToType,
-      quoteToType
+      quoteToType,
+      useCountdown,
+      countdownTimer,
+      isValidChar,
+      dispatch,
+      incrementKeysPressed,
+      setQuoteToType,
+      setUserTextInput,
+      setExcessQuoteToType,
+      excessQuoteToType,
+      incrementIncorrectKeys,
+      remakeQuoteString
     );
-
-    if (quoteToType.length === userTextInput.length) {
-      return;
-    }
-
-    if (useCountdown && countdownTimer <= 0) {
-      return;
-    }
-    if (isValidChar(e.key)) {
-      dispatch(incrementKeysPressed());
-    }
-
-    const nextCharIsSpace = quoteToType[userTextInput.length] === ' ';
-    // If the character that we are typing is supposed to be a space
-    if (nextCharIsSpace) {
-      // If the character typed IS NOT a space, adjust the quote to reflect the mistyped extra letters
-      if (e.key !== ' ') {
-        if (isValidChar(e.key)) {
-          dispatch(
-            setQuoteToType(
-              `${quoteToType.slice(0, userTextInput.length)}${
-                e.key
-              }${quoteToType.slice(userTextInput.length)}`
-            )
-          );
-          dispatch(setUserTextInput(userTextInput.concat(e.key)));
-          // the tilde (~) is used as a character that the algo knows to code as an excess letter
-          dispatch(setExcessQuoteToType(excessQuoteToType.concat('~')));
-          dispatch(incrementIncorrectKeys(1));
-        } else if (e.key === 'Backspace') {
-          // When Backspace is pressed but space SHOULD have been pressed
-
-          if (logicData.userInputWordLength > logicData.quoteWordLength) {
-            if (e.key === 'Backspace') {
-              dispatch(
-                setQuoteToType(
-                  remakeQuoteString(
-                    userTextInput,
-                    duplicateQuoteToType,
-                    quoteToType
-                  )
-                )
-              );
-              dispatch(
-                setExcessQuoteToType(
-                  excessQuoteToType.slice(0, excessQuoteToType.length - 1)
-                )
-              );
-              dispatch(
-                setUserTextInput(
-                  userTextInput.slice(0, userTextInput.length - 1)
-                )
-              );
-            }
-          } else {
-            if (e.key === 'Backspace') {
-              dispatch(
-                setUserTextInput(
-                  userTextInput.slice(0, userTextInput.length - 1)
-                )
-              );
-              dispatch(
-                setExcessQuoteToType(
-                  excessQuoteToType.slice(0, excessQuoteToType.length - 1)
-                )
-              );
-            }
-          }
-        }
-      } else {
-        // If the character is supposed to be a space and is a space, proceed as normal
-        dispatch(setUserTextInput(userTextInput.concat(e.key)));
-        dispatch(setExcessQuoteToType(excessQuoteToType.concat(e.key)));
-      }
-    } else {
-      if (e.key === 'Backspace') {
-        if (
-          // Delete all skipped letters when deleting to a prev word
-          excessQuoteToType.slice(-2, -1) === '%' ||
-          excessQuoteToType.slice(-2, -1) === '#'
-        ) {
-          let lastLetterIndex = excessQuoteToType.length - 2;
-          let lettersToDelete = 1;
-          while (
-            excessQuoteToType[lastLetterIndex] === '%' ||
-            excessQuoteToType[lastLetterIndex] === '#'
-          ) {
-            lastLetterIndex--;
-            lettersToDelete++;
-          }
-
-          dispatch(
-            setUserTextInput(
-              userTextInput.slice(0, userTextInput.length - lettersToDelete)
-            )
-          );
-          dispatch(
-            setExcessQuoteToType(
-              excessQuoteToType.slice(
-                0,
-                excessQuoteToType.length - lettersToDelete
-              )
-            )
-          );
-        } else {
-          dispatch(
-            setUserTextInput(userTextInput.slice(0, userTextInput.length - 1))
-          );
-          dispatch(
-            setExcessQuoteToType(
-              excessQuoteToType.slice(0, excessQuoteToType.length - 1)
-            )
-          );
-        }
-      } else if (e.key === ' ') {
-        // Pressing space will skip to the next word
-
-        // Don't allow pressing space when user is at the beginning of a word or on the last word
-        if (
-          userTextInput.slice(-1) === ' ' ||
-          userTextInput.length == 0 ||
-          logicData.currentWordNumber === duplicateQuoteToType.split(' ').length
-        ) {
-          return;
-        }
-        dispatch(
-          incrementIncorrectKeys(logicData.lettersRemainingInCurrentWord)
-        );
-        // # char indicates a space was pressed before the end of a word
-        let skipToNextWord = '#';
-        for (let i = 0; i < logicData.lettersRemainingInCurrentWord - 1; i++) {
-          // % char is viewed as a skipped character
-          skipToNextWord = skipToNextWord.concat('%');
-        }
-        skipToNextWord = skipToNextWord.concat(' ');
-
-        dispatch(setUserTextInput(userTextInput.concat(skipToNextWord)));
-        dispatch(
-          setExcessQuoteToType(excessQuoteToType.concat(skipToNextWord))
-        );
-      } else if (isValidChar(e.key)) {
-        dispatch(setUserTextInput(userTextInput.concat(e.key)));
-        dispatch(setExcessQuoteToType(excessQuoteToType.concat(e.key)));
-      }
-    }
   }
 };
 
